fb015545550d769d5db43bb0a3895b5735d0dcf8
CJ3010708 <CJ3010708@LAB01-PC36>
2024-03-27 10:28:35 -0300

    Adicionar arquivos de projeto.

---
+++ ATV5_CJ3010708/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ATV5_CJ3010708
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, temp;
+            int soma = 0;
+
+            for (a = 0; a < 3; a ++) {
+                Console.WriteLine("Digite um valor");
+                temp = int.Parse(Console.ReadLine());
+                if (temp < 0)
+                    break;
+                soma = soma + temp;
+            }
+            Console.WriteLine("A média é {0}", soma / 3);
+        }
+    }
+}
+
+
